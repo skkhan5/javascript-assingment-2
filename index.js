794c164a81db7665f9703d61c37c0d5b1f493c28
skkhan5 <yousafzai5588@gmail.com>
2024-04-17 01:38:01 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let userName = "abdul samad khan";
+let message = "hello world";
+alert(message)
+ alert(userName)
+alert("i m 24 year old")
+alert( "i m a certified mobile app developer")
+
+
+
+
+let i = "PIZZA\nPIZZ\nPIZ\nPI\nP "
+
+alert(i)
+
+
+
+ let email = "my emial address is "+ "yousafzai5588@gmail.com"
+
+ alert(email)
+
+
+
+
+ let book = "i m tying to learn the book a smarter way to learn javascript"
+
+ alert(book)
+
+
